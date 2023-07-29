efbd4c2fb6fd9a12c7408617d7ab1b9d2d0ec292
Stanislau Marudau <marudov.stas@gmail.com>
2023-07-29 16:48:01 +0300

    Styling for packed items added

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./index.css";
 
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
 ];
 
 const App = () => {
@@ -47,7 +47,7 @@ const Stats = () => {
 const Item = ({ item }) => {
   return (
     <li>
-      <span>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
         {item.quantity} {item.description}
       </span>
       <button>❌</button>
