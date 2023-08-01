0ae6561af6ceb4aff52335ef558646288190f1dc
Stanislau Marudau <marudov.stas@gmail.com>
2023-08-01 15:35:42 +0300

    Got rid of the redundant array

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import { useState } from "react";
 import "./index.css";
 
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: true },
-];
-
 const App = () => {
   const [items, setItems] = useState([]);
   const handleAddItems = (item) => {
