d6a462bff66bfc1b110cc7e796fa664805faa33e
Stanislau Marudau <marudov.stas@gmail.com>
2023-08-04 19:57:29 +0300

    List sorting added

--- src/App.js
+++ src/App.js
@@ -92,6 +92,13 @@ const PackingList = ({ items, onDeleteItem, onToggleItem }) => {
           />
         ))}
       </ul>
+      <div className="actions">
+        <select>
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by desscription</option>
+          <option value="packed">Sort by packed status</option>
+        </select>
+      </div>
     </div>
   );
 };
