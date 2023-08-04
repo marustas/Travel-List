3501e7f236bbc9bed096eeb3d9d52a087f2b3a60
Stanislau Marudau <marudov.stas@gmail.com>
2023-08-04 20:54:33 +0300

    added the feature for confirmation before deleting the list

--- src/App.js
+++ src/App.js
@@ -21,7 +21,12 @@ const App = () => {
   };
 
   const handleClearList = () => {
-    setItems([]);
+    const confirmed = window.confirm(
+      "Are you sure you want to delete all items?"
+    );
+    if (confirmed) {
+      setItems([]);
+    }
   };
   return (
     <div className="app">
